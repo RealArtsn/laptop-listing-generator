bc1f19b00b3f094d0b672a8fbd65a5d3f8fce294
RealArtsn <105236007+RealArtsn@users.noreply.github.com>
2024-06-14 15:26:58 -0400

    Generate listing title

--- script.js
+++ script.js
@@ -6,13 +6,42 @@ function generateText() {
     console.log('Generating output!')
     let titleOut = document.querySelector("#TitleOut");
     let conditionOut = document.querySelector("#ConditionOut");
-    let storageInclusion = ' Storage, ';
-    if (document.querySelector('#StorageSelect').value != 'none') {
-        storageInclusion = '';
+    // get specs from input
+    let model = document.querySelector('#modelName').value;
+    let cpu = document.querySelector('#cpuModel').value;
+    let memory = document.querySelector('#RAM').value + ' RAM';
+    let storage = document.querySelector('#StorageSelect').value + ' ' + document.querySelector('#StorageType').value;
+    let os = '';
+    let missing = []
+
+    let battery = document.querySelector('#batteryPercent').value + '% BATT';
+
+
+    // Generate and display title
+    titleOut.textContent = model + ' | ' + cpu + ' | ' + memory
+    if (document.querySelector('#StorageSelect').value == 'none') {
+        missing.push('STORAGE')
     }
-    titleOut.textContent = 'F3/C3: This unit has been POST tested for power, display, and the ability to boot to bios. \
-    Please refer to photos for wear and damages as the unit \
-    shows wear in multiple spots. This unit will not include' + storageInclusion + ' Operating System, or Power Adapter; these items will be needed for full functionality of the laptop. Comes w/30 Day Warranty'
+    else {
+        titleOut.textContent += ' | ' + storage;
+    }
+    if (document.querySelector('#os').checked) {
+        os = 'WIN 11 PRO'
+        titleOut.textContent += ' | ' + os;
+    }
+    else {
+        missing.push('OS')
+    }
+    if (document.querySelector('#battery').checked) {
+        titleOut.textContent += ' | ' + battery
+    }
+    else {
+        missing.push('BATTERY')
+    }
+    
+    titleOut.textContent += ' | NO ' + missing.join('/');
+    // Generate condition
+    conditionOut.textContent = ''
 }
 
 init()

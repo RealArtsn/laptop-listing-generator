10fe2245c90c58e6a8eda5e13ad652186ca2dadf
RealArtsn <105236007+RealArtsn@users.noreply.github.com>
2024-06-14 16:13:08 -0400

    Generate title in real time and calculate length

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function init() {
-    document.querySelector('#GenerateButton').addEventListener('click', generateText);
+    document.querySelector('#copyTitle').addEventListener('click', copyTitle);
+    document.querySelectorAll('input').forEach(e => {e.addEventListener('input', generateText)});
+    document.querySelectorAll('select').forEach(e => {e.addEventListener('input', generateText)});
+
 }
 
 function generateText() {
-    console.log('Generating output!')
+    console.log('Generating output!');
     let titleOut = document.querySelector("#TitleOut");
     let conditionOut = document.querySelector("#ConditionOut");
     // get specs from input
@@ -12,13 +15,13 @@ function generateText() {
     let memory = document.querySelector('#RAM').value + ' RAM';
     let storage = document.querySelector('#StorageSelect').value + ' ' + document.querySelector('#StorageType').value;
     let os = '';
-    let missing = []
+    let missing = [];
 
     let battery = document.querySelector('#batteryPercent').value + '% BATT';
 
 
     // Generate and display title
-    titleOut.textContent = model + ' | ' + cpu + ' | ' + memory
+    titleOut.textContent = model + ' | ' + cpu + ' | ' + memory;
     if (document.querySelector('#StorageSelect').value == 'none') {
         missing.push('STORAGE')
     }
@@ -26,22 +29,40 @@ function generateText() {
         titleOut.textContent += ' | ' + storage;
     }
     if (document.querySelector('#os').checked) {
-        os = 'WIN 11 PRO'
+        os = 'WIN11 PRO'
         titleOut.textContent += ' | ' + os;
     }
     else {
         missing.push('OS')
     }
     if (document.querySelector('#battery').checked) {
-        titleOut.textContent += ' | ' + battery
+        titleOut.textContent += ' | ' + battery;
     }
     else {
-        missing.push('BATTERY')
+        missing.push('BATTERY');
     }
     
-    titleOut.textContent += ' | NO ' + missing.join('/');
+    if (missing.length > 0) {
+        titleOut.textContent += ' | NO ' + missing.join('/');
+    }
+    let titleLength = document.querySelector('#titleLength');
+    titleLength.textContent = titleOut.textContent.length + '/80';
+    if (titleOut.textContent.length > 80) {
+        titleLength.style.color = 'Red'
+    }
+    else {
+        titleLength.style.color = 'Black'
+    }
+    // reset background after copy
+    titleOut.style.backgroundColor = '';
     // Generate condition
     conditionOut.textContent = ''
+}
+
+function copyTitle() {
+    let titleOut = document.querySelector('#TitleOut');
+    navigator.clipboard.writeText(titleOut.textContent);
+    titleOut.style.backgroundColor = 'LightGreen';
 }
 
 init()
